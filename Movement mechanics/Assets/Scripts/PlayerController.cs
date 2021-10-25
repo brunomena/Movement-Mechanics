c1ea9fdd6d089359c47709c41966a4bf5b780f11
brunomena <brunogrejanin@hotmail.com>
2021-10-25 14:11:04 -0300

    3D palette/Scripting
    Inicio do estudo dos codigos de tilemap e tilemap 3D

--- Movement mechanics/Assets/Scripts/PlayerController.cs
+++ Movement mechanics/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public Transform movePoint;
+    public Grid testGrid;
+    public Tilemap tileMap;
+    public TileBase yellowTile;
 
     // Start is called before the first frame update
     void Start()
     {
-        movePoint.parent = null;    
+        movePoint.parent = null;
+
     }
 
     // Update is called once per frame
@@ -23,6 +28,7 @@ public class PlayerController : MonoBehaviour
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
                 movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"),0f,0f);
+                tileMap.SetColor(Vector3Int.FloorToInt(transform.position), Color.yellow);
             }
 
             if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
